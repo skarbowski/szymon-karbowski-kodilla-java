8cbb3f48d1cf0fdeaf4107c41de40251a7627112
skarbowski <46647125+skarbowski@users.noreply.github.com>
2019-03-07 21:45:05 +0100

    07.03.2019

--- kodilla-tesing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-tesing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,17 +1,24 @@
 package com.kodilla.testing.collection;
-
 import java.util.ArrayList;
 
 public class OddNumbersExterminator {
 
-    public ArrayList<Integer> exterminate (ArrayList<Integer> numbers){
 
-           ArrayList<Integer> result = new ArrayList<>();
 
-           for(Integer a: numbers){
-               if (a%2==0)
-                   result.add(a);
-           }
-           return result;
+    public ArrayList<Integer> exterminate (ArrayList<Integer> numbers) {
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (Integer a : numbers) {
+            if (a % 2 == 0) result.add(a);
+        }
+        return result;
+
+
     }
 }
+
+
+
+
+
--- kodilla-tesing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-tesing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,4 +1,72 @@
 package com.kodilla.testing.collection;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.*;
+
+import java.util.ArrayList;
+
+import static java.util.OptionalDouble.empty;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CollectionTestSuite {
+
+    @Before
+    public void before() {
+        System.out.println("Test case : start");
+    }
+
+    @After
+    public void after() {
+        System.out.println("Test case : stop");
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Test Suite: begin");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Test Suite: end");
+    }
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList() {
+        //Given
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        //When
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        ArrayList<Integer> result = oddNumbersExterminator.exterminate(numbers);
+        //Then
+        assertTrue("custom error message",result.isEmpty());
+    }
+
+
+    @Test
+    public void testOddNumbersExterminatorNormalList() {
+
+        //Given
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(2);
+        numbers.add(3);
+        //When
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        ArrayList<Integer> result = oddNumbersExterminator.exterminate(numbers);
+        //Then
+        assertFalse("customer error message Normal List", result.isEmpty());
+        assertEquals(1, result.size());
+        for (Integer element : result){
+            assertTrue(element % 2 == 0);
+        }
+    }
+
+
+
+
+
+
 }
